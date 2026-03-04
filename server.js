3f69adf13a881402d233d7cca5fd79aeea6a80b3
saksham762007 <samrathore07062007@gmail.com>
2026-03-04 23:12:56 +0530

    file update

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = require("./src/index")
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ const express = require('express')
 const app = express();
 
 
-
 module.exports = app;
