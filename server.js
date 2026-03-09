e62606a2de6bf5a105386fff72ef408071285994
saksham762007 <samrathore07062007@gmail.com>
2026-03-09 20:48:22 +0530

    remove node_modules .env

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const app = require("./src/app");
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
 
+connectDB();
 
-app.listioner(3000, () => {
-    console.log('server is rumming on port 3000');
+app.get("/test", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
 })
