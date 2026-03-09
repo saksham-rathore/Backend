aad46e0f66d93ef2e58cebba80e5d1498086b72c
saksham762007 <samrathore07062007@gmail.com>
2026-03-09 23:59:50 +0530

    file update

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,8 +1,16 @@
-const user = require("../models/user-model.js");
+const User = require("../models/user-model.js");
 const express = require("express");
 
 const router = express.Router();
 
-
+router.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = await User.create({ name, email, password });
+        res.status(201).json({ user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 
 module.exports = router;
