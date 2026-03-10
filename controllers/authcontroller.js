d63b22321b1e541b5eb7b3e5cbe842925c72bb90
saksham762007 <samrathore07062007@gmail.com>
2026-03-10 23:02:32 +0530

    file update

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,16 +1,27 @@
 const User = require("../models/user-model.js");
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const newUser = await User.create({ name, email, password });
-        res.status(201).json({ user: newUser });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+async function userRegister(req, res) {
+  const { name, email, password } = req.body;
+
+  const isExists = await User.findOne({ email });
+  if (isExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  const user = await User.create({ name, email, password });
+  res.status(201).json({ user });
+}
+
+const hashedPassword = await bcrypt.hash(password, 10);
+
+const user = await User.create({
+  name,
+  email,
+  password: hashedPassword,
+});
 
 module.exports = router;
