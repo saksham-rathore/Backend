f1efb5225c94ca4c0e4b40c97d6c0151473beb0c
saksham762007 <samrathore07062007@gmail.com>
2026-04-18 15:58:32 +0530

    feat: create User model with password hashing and JWT authentication methods

--- src/models/user-partner.js
+++ src/models/user-partner.js
@@ -40,3 +40,33 @@ userSchema.pre("save", async function () {
 userSchema.methods.isPasswordCorrect = async function () {
     return await bcrypt.compare(Password, this.Password)
 }
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            email: this.email,
+            Password: this.Password
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
