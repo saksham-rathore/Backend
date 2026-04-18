65bb65dbd1e1f53342bf46787c0129476d2c66e9
saksham762007 <samrathore07062007@gmail.com>
2026-04-18 22:20:12 +0530

    feat: implement registerUser controller for user account creation

--- src/Controllers/User.model.js
+++ src/Controllers/User.model.js
@@ -1,23 +1,38 @@
+import { ApiError } from "../Utils/ApiError.js";
 import { User } from "../models/User.model.js";
 import { Jwt } from "jsonwebtoken";
 
 export const registerUser = async (res, req) => {
-    const {email, username, Password} = req.body;
+  const { email, username, Password } = req.body;
 
-    // check if Users exists
-    if (
-        [username, email, Password].some((field) =>
-        field?.trim() === "")
-    ) {
-        throw new ApiError(400, "All fields are required")
-    }
+  // check if Users exists
+  if ([username, email, Password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    const existedUser = await User.findone({
-        $or: [{username}, {email}],
-    });
+  const existedUser = await User.findone({
+    $or: [{ username }, { email }],
+  });
 
-    if (existedUser) {
-        throw new ApiError(400, "User already exists")
-    }
-    
-}
+  if (existedUser) {
+    throw new ApiError(400, "User already exists");
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    Password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-Password -refreshToken",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user")
+  }
+
+  return res
+  .status(201)
+  .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+};
