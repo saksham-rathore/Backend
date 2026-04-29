5efc6379e119d952ce2120162e493037269d6897
saksham762007 <samrathore07062007@gmail.com>
2026-04-29 18:20:39 +0530

    chore: update default server port from 3000 to 5000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import "dotenv/config";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
